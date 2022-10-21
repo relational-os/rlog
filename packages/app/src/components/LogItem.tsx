f66f2f7d0dd740fc32ee26e67183e2ea569f0b8d
plaguerider <mccommons@me.com>
2022-10-21 11:14:59 -0700

    UI Updates: Minor Layout and Styling
    - moved time ago to right side
    - shuffled order of tags & tag button
    - changed 'tag' to '+'
    - more color tweaks

--- packages/app/src/components/LogItem.tsx
+++ packages/app/src/components/LogItem.tsx
@@ -40,7 +40,7 @@ const LogItem = (data: any) => {
   return (
     
     <div
-      className="flex flex-col space-x-4 rounded-2xl py-1 px-3"
+      className="flex flex-col space-x-4 rounded-2xl py-2 px-4 "
       style={{ backgroundColor: `#${data.log.author.owner.slice(2, 8)}22` }}
     >
 
@@ -48,18 +48,18 @@ const LogItem = (data: any) => {
         <Link
           href={`https://mumbai.polygonscan.com/address/${data.log?.author?.owner}`}
         >
-          {<span className="u-color-1"
-                style={{ color: `#${data.log.author.owner.slice(2, 8)}` }}
+          {<span className="font-semibold"
           >{ensName}</span>}</Link>
-        {"-"}
-        <TimeAgo className="timeAgo" datetime={data.log.created * 1000}></TimeAgo>
-        <button
-          onClick={() => {
-            setIsTagging(true);
-          }}
-        >
-          tag
-        </button>
+
+
+        <span>
+          {data.log.tags.map((tag: any) => {
+            return <span className="ml-1.5 u-color-2"
+            style={{ color: `#${data.log.author.owner.slice(2, 8)}` }}
+            >#{tag.name}</span>
+          })}
+        </span>
+
         {isTagging && (
           <div>
             <select
@@ -90,26 +90,31 @@ const LogItem = (data: any) => {
           </div>
         )}
 
-        <span className="text-gray-400 mr-1 u-color-2">
-          {data.log.tags.map((tag: any) => {
-            return <span className="ml-1 u-color-2"
-            style={{ color: `#${data.log.author.owner.slice(2, 8)}` }}
-            >#{tag.name}</span>
-          })}
-        </span>
-      </div>
-      <div className="whitespace-pre-wrap text-base pb-1">
+        <button className="mr-auto u-color-2"
+          style={{ color: `#${data.log.author.owner.slice(2, 8)}` }}
+          onClick={() => {
+            setIsTagging(true);
+          }}
+        >
+          +
+        </button>
+
+        <TimeAgo className="text-gray-500 text-xs opacity-50" datetime={data.log.created * 1000}
+        ></TimeAgo>
+
+      </div> 
+
+      <div className="whitespace-pre-wrap text-base m-2">
         <Linkify>{data.log.data}</Linkify>
       </div>
+
         <style>{`
-          .timeAgo {}
-          .u-color-1 {
-            filter: saturate(10.0);
-            filter: brightness(1);            
-          }
           .u-color-2 {
-            filter: brightness(0.5); 
-            filter: saturate(0.6);            
+            filter: opacity(0.75);            
+          }
+          .u-color-2:hover {
+            filter: opacity(1);
+            cursor: pointer;
           }
       `}</style>
     </div>
